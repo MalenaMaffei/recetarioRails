8253f356fbcb3c6b700808b3f11b7ec08cad535b
MalenaMaffei <malenamaffei@hotmail.com>
2018-01-30 16:25:28 -0300

    se hace qyue el nombre de la categoria este con mayus y despues chequea que no exista

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,17 @@
 class Category < ApplicationRecord
     before_destroy :default_category
     has_many :recipes, dependent: :nullify
+    before_validation :titleize_name, on: :create
     validates :name, :presence => true, :length => { :minimum => 3 }, :uniqueness => true
-
 
     scope :sorted, -> { order("name ASC") }
 
 
+
+
+    def titleize_name
+      self.name = name.titleize
+    end
 
     def default_category
       self.recipes.each do |recipe|
