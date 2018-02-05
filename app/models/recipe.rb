b4fe7dd3ea480a5b4640c9bbfc9abb64d0a0d4b6
MalenaMaffei <malenamaffei@hotmail.com>
2018-02-05 16:42:17 -0300

    foto default

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -16,6 +16,7 @@ class Recipe < ApplicationRecord
 
 
 
+    # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ':style/default.jpeg'
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ':style/default.jpeg'
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
