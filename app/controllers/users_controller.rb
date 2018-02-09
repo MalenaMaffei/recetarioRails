4c2b095fff7517059229b1070ebb3cd3e80ccbd4
MalenaMaffei <malenamaffei@hotmail.com>
2018-02-08 22:18:30 -0300

    intentos dea rreglar devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   # before_action :authenticate_user!    
+   # before_action :authenticate_user!
   before_action :confirm_permissions, :except => [:show]
   def index
       @users = User.all
@@ -9,31 +9,31 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
+  #
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.save
+  #   valid_record?(@user, "Usuario '#{@user.username}' creado exitosamente.", users_path, 'new')
+  # end
 
-  def create
-    @user = User.new(user_params)
-    @user.save
-    valid_record?(@user, "Usuario '#{@user.username}' creado exitosamente.", users_path, 'new')
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update_attributes(user_params)
-    valid_record?(@user, "Usuario '#{@user.username}' actualizado exitosamente.", users_path, 'edit')
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    flash_redirect("Usuario '#{@user.username}' eliminado.", users_path)
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+  #
+  # def update
+  #   @user = User.find(params[:id])
+  #   @user.update_attributes(user_params)
+  #   valid_record?(@user, "Usuario '#{@user.username}' actualizado exitosamente.", users_path, 'edit')
+  # end
+  #
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   flash_redirect("Usuario '#{@user.username}' eliminado.", users_path)
+  # end
 
   private
   def user_params
